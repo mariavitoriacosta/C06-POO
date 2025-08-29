4aadf056c861f637a84c12abee573cdf9ec7398a
mariavitoriacosta <maria.costa@ges.inatel.br>
2025-08-28 21:59:36 -0300

    terminando o ex campo minado

--- aula-5/exercicio1/src/Main.java
+++ aula-5/exercicio1/src/Main.java
@@ -1,22 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
         boolean [][] campo = new boolean[2][2];
+        boolean ganhou = true;
 
-        int bomba = rand.nextInt(3);
+        int bombaX = rand.nextInt(2);
+        int bombaY = rand.nextInt(2);
+
+        campo[bombaX][bombaY] = true;
+
+        System.out.println("Bem-vindo ao Campo Minado!");
+        System.out.println("Digite a posição X e Y para jogar:");
 
         Scanner entrada = new Scanner(System.in);
 
-
         for(int i = 0; i < 3; i++){
-            int digitado = entrada.nextInt();
+            int digitadoX = entrada.nextInt();
+            int digitadoY = entrada.nextInt();
+            if(campo[digitadoX][digitadoY] == true) {
+                System.out.println("Você explodiu :( Tente mais tarde!");
+                ganhou = false;
+                break;
+            }
+            else if(i != 2){
+                System.out.println("Você está salvo! Por enquanto...");
+            }
         }
 
+        if(ganhou == true){
+            System.out.println("Você ganhou! Parabéns!!");
+        }
 
     }
 }
